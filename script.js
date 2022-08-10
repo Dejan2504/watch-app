7a454d4b1f7184e23e20b59628f330fd7d5afc45
Dejan2504 <krajisnikdejan@gmail.com>
2022-08-10 22:15:35 +0200

    Fixed a bug for background image

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const updateTime = function(){
     timeContainer.innerHTML = date.toLocaleTimeString();
     dateContainer.innerHTML = date.toLocaleDateString();
 
-    if(date.getHours < 7 && date.getHours > 18){
+    if(date.getHours() < 7 || date.getHours() > 18){
         body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1505771215590-c5fa0aec29b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80)';
         timeContainer.style.color = 'white';
         dateContainer.style.color = 'white';
