bd18216ebd5706c67e1d4de24777fab385b9d390
Dejan2504 <krajisnikdejan@gmail.com>
2022-08-10 16:55:24 +0200

    Fixed background images

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ const updateTime = function(){
     dateContainer.innerHTML = date.toLocaleDateString();
 
     if(date.getHours < 7 && date.getHours > 18){
-        body.style.backgroundImage = 'url(img/biel-morro-J_F_003jcEQ-unsplash.jpg)';
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1505771215590-c5fa0aec29b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80)';
         timeContainer.style.color = 'white';
         dateContainer.style.color = 'white';
     } else{
-        body.style.backgroundImage = 'url(img/diana-serbichenko-5dpSx2hBtj0-unsplash.jpg)';
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1659342313448-3f5115622e83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)';
         timeContainer.style.color = '#fefbd8';
         dateContainer.style.color = '#fefbd8';
     }
